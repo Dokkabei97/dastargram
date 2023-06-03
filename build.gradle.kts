878651cb14018a90dfeb1162f440b86f8c5b1236
Dokkabei97 <wkdrn970@naver.com>
2023-06-03 14:23:31 +0900

    feat: kotest, 및 application.yml 분리

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "com.d1t"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
@@ -28,11 +28,18 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+//    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    implementation("io.kotest:kotest-property-jvm:5.6.1")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.6.1")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.6.1")
+    implementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+
+    testImplementation("io.mockk:mockk:1.13.2")
 }
 
 tasks.withType<KotlinCompile> {
