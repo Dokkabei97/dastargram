3ba29f9508accd766a6548a35f0425a359f2f1e3
Dokkabei97 <wkdrn970@naver.com>
2023-06-10 19:10:01 +0900

    feat: JPA 공통 모듈

---
+++ src/main/kotlin/com/d1t/dastargram/global/common/AbstractEntity.kt
@@ -0,0 +1,19 @@
+package com.d1t.dastargram.global.common
+
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class AbstractEntity {
+
+    @CreatedDate
+    lateinit var createdAt: LocalDateTime
+
+    @LastModifiedDate
+    lateinit var updatedAt: LocalDateTime
+}
