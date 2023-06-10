7fe85a53932b21e8c695176262ac690573729953
Dokkabei97 <wkdrn970@naver.com>
2023-06-10 23:51:03 +0900

    refactor: 정규식 상수로 변경

--- src/main/kotlin/com/d1t/dastargram/domain/user/domain/User.kt
+++ src/main/kotlin/com/d1t/dastargram/domain/user/domain/User.kt
@@ -21,7 +21,12 @@ class User(
         var nickname: String,
 
         ) : AbstractEntity() {
+
     companion object {
+        private const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
+        private const val PASSWORD_REGEX = "^[a-zA-Z0-9!@#$%^&*()?_~]{8,15}$"
+        private const val NICKNAME_REGEX = "^[가-힣a-zA-Z0-9]{2,10}$"
+
         fun create(email: String, password: String, nickname: String): User {
             validateEmail(email)
             validatePassword(password)
@@ -32,19 +37,19 @@ class User(
         private fun validateEmail(email: String) {
             require(email.isNotBlank()) { "이메일은 필수입니다." }
             check(email.length <= 30) { "이메일은 30자리 이하여야 합니다." }
-            check(email.matches(Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"))) { "이메일 형식이 올바르지 않습니다." }
+            check(email.matches(Regex(EMAIL_REGEX))) { "이메일 형식이 올바르지 않습니다." }
         }
 
         private fun validatePassword(password: String) {
             require(password.isNotBlank()) { "비밀번호는 필수입니다." }
             check(password.length in 8..15) { "비밀번호는 8~15자리여야 합니다." }
-            check(password.matches(Regex("^[a-zA-Z0-9!@#$%^&*()?_~]{8,15}$"))) { "비밀번호는 영문 대소문자, 숫자, 특수문자(!@#$%^&*()?_~)로만 구성되어야 합니다." }
+            check(password.matches(Regex(PASSWORD_REGEX))) { "비밀번호는 영문 대소문자, 숫자, 특수문자(!@#$%^&*()?_~)로만 구성되어야 합니다." }
         }
 
         private fun validateNickname(nickname: String) {
             require(nickname.isNotBlank()) { "닉네임은 필수입니다." }
             check(nickname.length in 2..10) { "닉네임은 2~10자리여야 합니다." }
-            check(nickname.matches(Regex("^[가-힣a-zA-Z0-9]{2,10}$"))) { "닉네임은 한글, 영문, 숫자로만 구성되어야 합니다." }
+            check(nickname.matches(Regex(NICKNAME_REGEX))) { "닉네임은 한글, 영문, 숫자로만 구성되어야 합니다." }
         }
     }
 
