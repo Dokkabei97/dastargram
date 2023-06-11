3b8c6f23074fad09c069c2adb5a44a58041d7f71
Dokkabei97 <wkdrn970@naver.com>
2023-06-11 15:34:43 +0900

    feat: User Entity 추가 기능 구현
    - name, profileImage, followerCount, followingCount 구현
    
    - 테스트 코드 추가 구현

--- src/main/kotlin/com/d1t/dastargram/domain/user/domain/User.kt
+++ src/main/kotlin/com/d1t/dastargram/domain/user/domain/User.kt
@@ -20,18 +20,32 @@ class User(
         @Column(name = "nickname")
         var nickname: String,
 
+        @Column(name = "name")
+        var name: String,
+
+        @Column(name = "profile_image")
+        var profileImage: String?,
+
+        @Column(name = "follower_count")
+        var followerCount: Int = 0,
+
+        @Column(name = "following_count")
+        var followingCount: Int = 0,
+
         ) : AbstractEntity() {
 
     companion object {
         private const val EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
         private const val PASSWORD_REGEX = "^[a-zA-Z0-9!@#$%^&*()?_~]{8,15}$"
         private const val NICKNAME_REGEX = "^[가-힣a-zA-Z0-9]{2,10}$"
+        private const val NAME_REGEX = "^[가-힣a-zA-Z]{2,10}$"
 
-        fun create(email: String, password: String, nickname: String): User {
+        fun create(email: String, password: String, nickname: String, name: String): User {
             validateEmail(email)
             validatePassword(password)
             validateNickname(nickname)
-            return User(null, email, password, nickname)
+            validateName(name)
+            return User(null, email, password, nickname, name, null)
         }
 
         private fun validateEmail(email: String) {
@@ -51,6 +65,20 @@ class User(
             check(nickname.length in 2..10) { "닉네임은 2~10자리여야 합니다." }
             check(nickname.matches(Regex(NICKNAME_REGEX))) { "닉네임은 한글, 영문, 숫자로만 구성되어야 합니다." }
         }
+
+        private fun validateName(name: String) {
+            require(name.isNotBlank()) { "이름은 필수입니다." }
+            check(name.length in 2..10) { "이름은 2~10자리여야 합니다." }
+            check(name.matches(Regex(NAME_REGEX))) { "이름은 한글, 영문으로만 구성되어야 합니다." }
+        }
+
+        private fun validateFollowingCount(followingCount: Int) {
+            check(followingCount > 0) { "팔로잉 수는 0보다 작을 수 없습니다." }
+        }
+
+        private fun validateFollowerCount(followerCount: Int) {
+            check(followerCount > 0) { "팔로워 수는 0보다 작을 수 없습니다." }
+        }
     }
 
     fun updatePassword(password: String) {
@@ -61,5 +89,32 @@ class User(
     fun updateNickname(nickname: String) {
         validateNickname(nickname)
         this.nickname = nickname
+    }
+
+    fun updateName(name: String) {
+        validateName(name)
+        this.name = name
+    }
+
+    fun updateProfileImage(profileImage: String) {
+        this.profileImage = profileImage
+    }
+
+    fun increaseFollowerCount() {
+        this.followerCount++
+    }
+
+    fun decreaseFollowerCount() {
+        validateFollowerCount(this.followerCount)
+        this.followerCount--
+    }
+
+    fun increaseFollowingCount() {
+        this.followingCount++
+    }
+
+    fun decreaseFollowingCount() {
+        validateFollowingCount(this.followingCount)
+        this.followingCount--
     }
 }
--- src/test/kotlin/com/d1t/dastargram/domain/user/domain/UserTest.kt
+++ src/test/kotlin/com/d1t/dastargram/domain/user/domain/UserTest.kt
@@ -9,17 +9,21 @@ class UserTest : DescribeSpec({
     describe("User.create") {
         context("이메일, 비밀번호, 닉네임이 주어졌을 때") {
             it("유저를 생성한다.") {
-                val user = User.create(TEST_EMAIL, TEST_PASSWORD, TEST_NICKNAME)
+                val user = User.create(TEST_EMAIL, TEST_PASSWORD, TEST_NICKNAME, TEST_NAME)
                 user.email shouldBe TEST_EMAIL
                 user.password shouldBe TEST_PASSWORD
                 user.nickname shouldBe TEST_NICKNAME
+                user.name shouldBe TEST_NAME
+                user.profileImage shouldBe null
+                user.followerCount shouldBe 0
+                user.followingCount shouldBe 0
             }
         }
 
         context("이메일이 주어지지 않았을 때") {
             it("IllegalArgumentException을 던진다.") {
                 val exception = shouldThrow<IllegalArgumentException> {
-                    User.create("", TEST_PASSWORD, TEST_NICKNAME)
+                    User.create("", TEST_PASSWORD, TEST_NICKNAME, TEST_NAME)
                 }
                 exception.message shouldBe "이메일은 필수입니다."
             }
@@ -28,7 +32,7 @@ class UserTest : DescribeSpec({
         context("이메일이 30자리를 초과했을 때") {
             it("IllegalStateException을 던진다.") {
                 val exception = shouldThrow<IllegalStateException> {
-                    User.create("a".repeat(31), TEST_PASSWORD, TEST_NICKNAME)
+                    User.create("a".repeat(31), TEST_PASSWORD, TEST_NICKNAME, TEST_NAME)
                 }
                 exception.message shouldBe "이메일은 30자리 이하여야 합니다."
             }
@@ -37,7 +41,7 @@ class UserTest : DescribeSpec({
         context("이메일 형식이 올바르지 않았을 때") {
             it("IllegalStateException을 던진다.") {
                 val exception = shouldThrow<IllegalStateException> {
-                    User.create("testemail.com", TEST_PASSWORD, TEST_NICKNAME)
+                    User.create("testemail.com", TEST_PASSWORD, TEST_NICKNAME, TEST_NAME)
                 }
                 exception.message shouldBe "이메일 형식이 올바르지 않습니다."
             }
@@ -46,7 +50,7 @@ class UserTest : DescribeSpec({
         context("비밀번호가 주어지지 않았을 때") {
             it("IllegalArgumentException을 던진다.") {
                 val exception = shouldThrow<IllegalArgumentException> {
-                    User.create(TEST_EMAIL, "", TEST_NICKNAME)
+                    User.create(TEST_EMAIL, "", TEST_NICKNAME, TEST_NAME)
                 }
                 exception.message shouldBe "비밀번호는 필수입니다."
             }
@@ -55,7 +59,7 @@ class UserTest : DescribeSpec({
         context("비밀번호가 8자리 미만일 때") {
             it("IllegalStateException을 던진다.") {
                 val exception = shouldThrow<IllegalStateException> {
-                    User.create(TEST_EMAIL, "test", TEST_NICKNAME)
+                    User.create(TEST_EMAIL, "test", TEST_NICKNAME, TEST_NAME)
                 }
                 exception.message shouldBe "비밀번호는 8~15자리여야 합니다."
             }
@@ -64,7 +68,7 @@ class UserTest : DescribeSpec({
         context("비밀번호가 15자리를 초과했을 때") {
             it("IllegalStateException을 던진다.") {
                 val exception = shouldThrow<IllegalStateException> {
-                    User.create(TEST_EMAIL, "a".repeat(16), TEST_NICKNAME)
+                    User.create(TEST_EMAIL, "a".repeat(16), TEST_NICKNAME, TEST_NAME)
                 }
                 exception.message shouldBe "비밀번호는 8~15자리여야 합니다."
             }
@@ -73,7 +77,7 @@ class UserTest : DescribeSpec({
         context("비밀번호 사용 불가 특수문자가 있을 때") {
             it("IllegalStateException을 던진다.") {
                 val exception = shouldThrow<IllegalStateException> {
-                    User.create(TEST_EMAIL, "$TEST_PASSWORD+", TEST_NICKNAME)
+                    User.create(TEST_EMAIL, "$TEST_PASSWORD+", TEST_NICKNAME, TEST_NAME)
                 }
                 exception.message shouldBe "비밀번호는 영문 대소문자, 숫자, 특수문자(!@#\$%^&*()?_~)로만 구성되어야 합니다."
             }
@@ -82,7 +86,7 @@ class UserTest : DescribeSpec({
         context("닉네임이 주어지지 않았을 때") {
             it("IllegalArgumentException을 던진다.") {
                 val exception = shouldThrow<IllegalArgumentException> {
-                    User.create(TEST_EMAIL, TEST_PASSWORD, "")
+                    User.create(TEST_EMAIL, TEST_PASSWORD, "", TEST_NAME)
                 }
                 exception.message shouldBe "닉네임은 필수입니다."
             }
@@ -91,7 +95,7 @@ class UserTest : DescribeSpec({
         context("닉네임이 2자리 미만일 때") {
             it("IllegalStateException을 던진다.") {
                 val exception = shouldThrow<IllegalStateException> {
-                    User.create(TEST_EMAIL, TEST_PASSWORD, "홍")
+                    User.create(TEST_EMAIL, TEST_PASSWORD, "홍", TEST_NAME)
                 }
                 exception.message shouldBe "닉네임은 2~10자리여야 합니다."
             }
@@ -100,7 +104,7 @@ class UserTest : DescribeSpec({
         context("닉네임이 10자리를 초과했을 때") {
             it("IllegalStateException을 던진다.") {
                 val exception = shouldThrow<IllegalStateException> {
-                    User.create(TEST_EMAIL, TEST_PASSWORD, "a".repeat(11))
+                    User.create(TEST_EMAIL, TEST_PASSWORD, "a".repeat(11), TEST_NAME)
                 }
                 exception.message shouldBe "닉네임은 2~10자리여야 합니다."
             }
@@ -109,29 +113,57 @@ class UserTest : DescribeSpec({
         context("닉네임에 특수문자가 포함되었을 때") {
             it("IllegalStateException을 던진다.") {
                 val exception = shouldThrow<IllegalStateException> {
-                    User.create(TEST_EMAIL, TEST_PASSWORD, "$TEST_NICKNAME!")
+                    User.create(TEST_EMAIL, TEST_PASSWORD, "$TEST_NICKNAME!", TEST_NAME)
                 }
                 exception.message shouldBe "닉네임은 한글, 영문, 숫자로만 구성되어야 합니다."
+            }
+        }
+
+        context("이름이 주어지지 않았을 때") {
+            it("IllegalArgumentException을 던진다.") {
+                val exception = shouldThrow<IllegalArgumentException> {
+                    User.create(TEST_EMAIL, TEST_PASSWORD, TEST_NICKNAME, "")
+                }
+                exception.message shouldBe "이름은 필수입니다."
+            }
+        }
+
+        context("이름이 2자리 미만일 때") {
+            it("IllegalStateException을 던진다.") {
+                val exception = shouldThrow<IllegalStateException> {
+                    User.create(TEST_EMAIL, TEST_PASSWORD, TEST_NICKNAME, "홍")
+                }
+                exception.message shouldBe "이름은 2~10자리여야 합니다."
+            }
+        }
+
+        context("이름이 10자리를 초과했을 때") {
+            it("IllegalStateException을 던진다.") {
+                val exception = shouldThrow<IllegalStateException> {
+                    User.create(TEST_EMAIL, TEST_PASSWORD, TEST_NICKNAME, "a".repeat(11))
+                }
+                exception.message shouldBe "이름은 2~10자리여야 합니다."
+            }
+        }
+
+        context("이름에 특수문자가 포함되었을 때") {
+            it("IllegalStateException을 던진다.") {
+                val exception = shouldThrow<IllegalStateException> {
+                    User.create(TEST_EMAIL, TEST_PASSWORD, TEST_NICKNAME, "$TEST_NAME!")
+                }
+                exception.message shouldBe "이름은 한글, 영문으로만 구성되어야 합니다."
             }
         }
     }
 
     describe("User.update") {
-        val user = User.create(TEST_EMAIL, TEST_PASSWORD, TEST_NICKNAME)
+        val user = User.create(TEST_EMAIL, TEST_PASSWORD, TEST_NICKNAME, TEST_NAME)
 
         context("비밀번호를 변겅하면") {
             it("비밀번호가 변경된다.") {
                 val newPassword = "test1234!"
                 user.updatePassword(newPassword)
                 user.password shouldBe newPassword
-            }
-        }
-
-        context("닉네임을 변경하면") {
-            it("닉네임이 변경된다.") {
-                val newNickname = "김길동"
-                user.updateNickname(newNickname)
-                user.nickname shouldBe newNickname
             }
         }
 
@@ -171,6 +203,14 @@ class UserTest : DescribeSpec({
             }
         }
 
+        context("닉네임을 변경하면") {
+            it("닉네임이 변경된다.") {
+                val newNickname = "조선도적"
+                user.updateNickname(newNickname)
+                user.nickname shouldBe newNickname
+            }
+        }
+
         context("닉네임이 주어지지 않았을 때") {
             it("IllegalArgumentException을 던진다.") {
                 val exception = shouldThrow<IllegalArgumentException> {
@@ -206,11 +246,121 @@ class UserTest : DescribeSpec({
                 exception.message shouldBe "닉네임은 한글, 영문, 숫자로만 구성되어야 합니다."
             }
         }
+
+        context("이름을 변경하면") {
+            it("이름이 변경된다.") {
+                val newName = "김길동"
+                user.updateName(newName)
+                user.name shouldBe newName
+            }
+        }
+
+        context("이름이 주어지지 않았을 때") {
+            it("IllegalArgumentException을 던진다.") {
+                val exception = shouldThrow<IllegalArgumentException> {
+                    user.updateName("")
+                }
+                exception.message shouldBe "이름은 필수입니다."
+            }
+        }
+
+        context("이름이 2자리 미만일 때") {
+            it("IllegalStateException을 던진다.") {
+                val exception = shouldThrow<IllegalStateException> {
+                    user.updateName("홍")
+                }
+                exception.message shouldBe "이름은 2~10자리여야 합니다."
+            }
+        }
+
+        context("이름이 10자리를 초과했을 때") {
+            it("IllegalStateException을 던진다.") {
+                val exception = shouldThrow<IllegalStateException> {
+                    user.updateName("a".repeat(11))
+                }
+                exception.message shouldBe "이름은 2~10자리여야 합니다."
+            }
+        }
+
+        context("이름에 특수문자가 포함되었을 때") {
+            it("IllegalStateException을 던진다.") {
+                val exception = shouldThrow<IllegalStateException> {
+                    user.updateName("$TEST_NAME!")
+                }
+                exception.message shouldBe "이름은 한글, 영문으로만 구성되어야 합니다."
+            }
+        }
+
+        context("이름이 한글, 영문이 아닐 때") {
+            it("IllegalStateException을 던진다.") {
+                val exception = shouldThrow<IllegalStateException> {
+                    user.updateName("홍길동1")
+                }
+                exception.message shouldBe "이름은 한글, 영문으로만 구성되어야 합니다."
+            }
+        }
+
+        context("팔로워 수 증가") {
+            it("팔로워 수가 증가한다.") {
+                user.increaseFollowerCount()
+                user.followerCount shouldBe 1
+            }
+        }
+
+        context("팔로워 수 감소") {
+            it("팔로워 수가 감소한다.") {
+                user.decreaseFollowerCount()
+                user.followerCount shouldBe 0
+            }
+        }
+
+        context("팔로워 수 감소할 때 팔로워 수가 0이면") {
+            it("IllegalStateException을 던진다.") {
+                val exception = shouldThrow<IllegalStateException> {
+                    user.decreaseFollowerCount()
+                }
+                exception.message shouldBe "팔로워 수는 0보다 작을 수 없습니다."
+                user.followerCount shouldBe 0
+            }
+        }
+
+        context("팔로잉 수 증가") {
+            it("팔로잉 수가 증가한다.") {
+                user.increaseFollowingCount()
+                user.followingCount shouldBe 1
+            }
+        }
+
+        context("팔로잉 수 감소") {
+            it("팔로잉 수가 감소한다.") {
+                user.decreaseFollowingCount()
+                user.followingCount shouldBe 0
+            }
+        }
+
+        context("팔로잉 수 감소할 때 팔로잉 수가 0이면") {
+            it("IllegalStateException을 던진다.") {
+                val exception = shouldThrow<IllegalStateException> {
+                    user.decreaseFollowingCount()
+                }
+                exception.message shouldBe "팔로잉 수는 0보다 작을 수 없습니다."
+                user.followingCount shouldBe 0
+            }
+        }
+
+        context("프로필 사진을 변경하면") {
+            it("프로필 사진이 변경된다.") {
+                val newProfileImage = "https://example.com/profile.jpg"
+                user.updateProfileImage(newProfileImage)
+                user.profileImage shouldBe newProfileImage
+            }
+        }
     }
 }) {
     companion object {
         const val TEST_EMAIL = "test@example.com"
         const val TEST_PASSWORD = "test123T!"
-        const val TEST_NICKNAME = "홍길동"
+        const val TEST_NICKNAME = "생계형강도"
+        const val TEST_NAME = "홍길동"
     }
 }
