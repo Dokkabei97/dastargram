8d56787cd79de5dfb8f2e9f571e4c54073d7e2e5
Dokkabei97 <wkdrn970@naver.com>
2023-06-12 19:39:23 +0900

    test: 회원 가입 테스트 코드

--- src/test/kotlin/com/d1t/dastargram/domain/member/api/MemberControllerTest.kt
+++ src/test/kotlin/com/d1t/dastargram/domain/member/api/MemberControllerTest.kt
@@ -5,30 +5,26 @@ import com.d1t.dastargram.domain.member.TestMemberArgument.Companion.TEST_NAME
 import com.d1t.dastargram.domain.member.TestMemberArgument.Companion.TEST_NICKNAME
 import com.d1t.dastargram.domain.member.TestMemberArgument.Companion.TEST_PASSWORD
 import com.d1t.dastargram.domain.member.application.MemberFacade
-import com.d1t.dastargram.domain.member.domain.Member
 import com.d1t.dastargram.domain.member.dto.MemberRequest.*
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import io.kotest.core.spec.style.AnnotationSpec
-import io.mockk.Runs
-import io.mockk.every
-import io.mockk.just
-import io.mockk.mockk
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.web.servlet.HttpEncodingAutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
+import org.springframework.test.web.servlet.setup.MockMvcBuilders.*
 
 @WebMvcTest(MemberController::class)
 @Import(HttpEncodingAutoConfiguration::class)
-class MemberControllerTest : AnnotationSpec() {
-    private val member = mockk<Member>()
-    private val memberFacade = mockk<MemberFacade>()
-    private val mockMvc: MockMvc = MockMvcBuilders.standaloneSetup(MemberController(memberFacade)).build()
+class MemberControllerTest {
+    // @WebMvcTest에 Kotest 사용시 한글 깨짐 이슈 존재
+    @MockBean lateinit var memberFacade: MemberFacade
+    @Autowired lateinit var mockMvc: MockMvc
 
     @Test
     fun `회원가입 성공`() {
@@ -39,16 +35,16 @@ class MemberControllerTest : AnnotationSpec() {
                 TEST_NAME
         )
 
-        every { memberFacade.signUp(signUpMemberRequest) } returns member
+        val json = jacksonObjectMapper().writeValueAsString(signUpMemberRequest)
 
         mockMvc.perform(post("/api/v1/members")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(jacksonObjectMapper().writeValueAsString(signUpMemberRequest))
+                .content(json)
                 .characterEncoding("utf-8"))
                 .andExpect(status().isOk)
                 .andExpect(content().string("회원가입 성공"))
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.message").value("회원가입 성공"))
-        // TODO:  테스트 실제 결과값이 ???? ??로 나와서 실패함
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN))
+                .andExpect(content().string("회원가입 성공"))
+        // TODO 공통 Response 개발 완료 후 Controller Return 바뀌면 수정 필요
     }
 }
