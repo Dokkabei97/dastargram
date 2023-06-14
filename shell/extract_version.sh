da9bfc0d980858fe31664fcbb57639513789e219
Dokkabei97 <wkdrn970@naver.com>
2023-06-14 21:56:29 +0900

    feat: cicd 구성6

--- shell/extract_version.sh
+++ shell/extract_version.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-JAR_NAME=$(ls /home/runner/work/dastargram/dastargram/build/libs/*.jar | grep -v plain)
+HOME=$1
+
+JAR_NAME=$(ls $HOME/build/libs/*.jar | grep -v plain)
 VERSION=$(echo $JAR_NAME | grep -o -E "[0-9]+\.[0-9]+\.[0-9]+")
 echo $VERSION
