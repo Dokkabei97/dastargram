13c1d972b7ebb2e6b7337c23bdf9b09e6fcbdb28
Dokkabei97 <wkdrn970@naver.com>
2023-06-26 20:12:35 +0900

    test: 테스트코드 수정

--- src/test/kotlin/com/d1t/dastargram/domain/member/domain/MemberServiceImplTest.kt
+++ src/test/kotlin/com/d1t/dastargram/domain/member/domain/MemberServiceImplTest.kt
@@ -7,58 +7,52 @@ import com.d1t.dastargram.domain.member.TestMemberArgument.Companion.TEST_PASSWO
 import com.d1t.dastargram.domain.member.dto.MemberRequest
 import com.d1t.dastargram.domain.member.dto.MemberResponse
 import io.kotest.core.spec.style.BehaviorSpec
-import io.mockk.*
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 
 class MemberServiceImplTest : BehaviorSpec({
-    val member = mockk<Member>()
-    val memberResponse = mockk<MemberResponse.MemberPublicResponse>()
-    val memberReader = mockk<MemberReader>()
-    val memberStore = mockk<MemberStore>()
-
+    val memberStore: MemberStore = mockk()
+    val memberReader: MemberReader = mockk()
     val memberService = MemberServiceImpl(memberStore, memberReader)
 
-    given("회원가입 요청이 들어온다") {
-        val request = MemberRequest.SignUpMemberRequest(
-                TEST_EMAIL,
-                TEST_PASSWORD,
-                TEST_NICKNAME,
-                TEST_NAME
-        )
-        `when`("회원가입을 한다") {
-            every { memberReader.isExistsByEmail(any()) } returns false
-            every { memberReader.isExistsByNickname(any()) } returns false
-            every { memberStore.create(any()) } returns member
+    given("회원가입 요청") {
+        val memberRequest = MemberRequest.SignUpMemberRequest(TEST_EMAIL, TEST_PASSWORD, TEST_NICKNAME, TEST_NAME)
+        val member = Member.create(TEST_EMAIL, TEST_PASSWORD, TEST_NICKNAME, TEST_NAME)
+        val memberResponse = MemberResponse.MemberPublicResponse(TEST_NICKNAME, TEST_NAME, null)
 
-            memberService.signUp(request)
-            then("회원가입이 완료된다") {
-                verify { memberReader.isExistsByEmail(any()) }
-                verify { memberReader.isExistsByNickname(any()) }
-                verify { memberStore.create(any()) }
+        every { memberReader.isExistsByEmail(any()) } returns false
+        every { memberReader.isExistsByNickname(any()) } returns false
+        every { memberStore.create(any()) } returns member
+
+        `when`("회원가입 요청") {
+            val result = memberService.signUp(memberRequest)
+
+            then("회원가입 성공") {
+                result shouldBe memberResponse
+
+                verify(exactly = 1) { memberStore.create(any()) }
             }
         }
     }
 
-    given("업데이트 요청이 들어온다") {
-        val request = MemberRequest.UpdateMemberRequest(
-                1L,
-                TEST_PASSWORD,
-                TEST_NICKNAME,
-                TEST_NAME,
-                "profileImage"
-        )
-        `when`("업데이트를 한다") {
-            every { memberReader.findById(any()) } returns Member.create(
-                    TEST_EMAIL,
-                    TEST_PASSWORD,
-                    TEST_NICKNAME,
-                    TEST_NAME
-            )
-            every { memberReader.isExistsByNickname(any()) } returns false
+    given("회원정보 수정 요청") {
+        val memberId = 1L
+        val memberRequest = MemberRequest.UpdateMemberRequest(memberId, TEST_PASSWORD, TEST_NICKNAME, TEST_NAME, null)
+        val member = Member.create(TEST_EMAIL, TEST_PASSWORD, TEST_NICKNAME, TEST_NAME)
+        val memberResponse = MemberResponse.MemberPublicResponse(TEST_NICKNAME, TEST_NAME, null)
 
-            memberService.update(request)
-            then("업데이트가 완료된다") {
-                verify { memberReader.findById(any()) }
-                verify { memberReader.isExistsByNickname(any()) }
+        every { memberReader.findById(any()) } returns member
+        every { memberReader.isExistsByNickname(any()) } returns false
+
+        `when`("회원정보 수정 요청") {
+            val result = memberService.update(memberRequest)
+
+            then("회원정보 수정 성공") {
+                result shouldBe memberResponse
+
+                verify(exactly = 1) { memberReader.findById(any()) }
             }
         }
     }
