c9de0e44eb2a42b2d5af0272c2ede1d7313760d2
Dokkabei97 <wkdrn970@naver.com>
2023-07-02 21:31:48 +0900

    feat: security 임시 설정

--- src/main/kotlin/com/d1t/dastargram/global/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/d1t/dastargram/global/config/security/SecurityConfig.kt
@@ -2,9 +2,11 @@ package com.d1t.dastargram.global.config.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +14,15 @@ class SecurityConfig {
 
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+                .csrf { it.disable() }
+                .httpBasic { it.disable()}
+                .formLogin { it.disable() }
+
+        return http.build()
+    }
 
 }
