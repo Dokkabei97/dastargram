36f365445d0f3843bf15caa9228d6edac611e822
Dokkabei97 <wkdrn970@naver.com>
2023-07-11 07:00:20 +0900

    feat: 레디스 함수 설명 주석 추가

--- src/main/kotlin/com/d1t/dastargram/global/config/redis/RedisConfiguration.kt
+++ src/main/kotlin/com/d1t/dastargram/global/config/redis/RedisConfiguration.kt
@@ -10,6 +10,15 @@ import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
+/**
+ * Spring Data Redis 설정
+ * template 함수
+ * .opsForValue() : Redis의 String 타입 데이터를 저장하고 조회하는 기능
+ * .opsForHash() : Redis의 Hash 타입 데이터를 저장하고 조회하는 기능
+ * .opsForList() : Redis의 List 타입 데이터를 저장하고 조회하는 기능
+ * .opsForSet() : Redis의 Set 타입 데이터를 저장하고 조회하는 기능
+ * .opsForZSet() : Redis의 ZSet 타입 데이터를 저장하고 조회하는 기능
+ */
 @Configuration
 @EnableRedisRepositories
 class RedisConfiguration {
