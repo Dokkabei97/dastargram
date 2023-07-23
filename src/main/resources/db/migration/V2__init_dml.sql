1f69488d08a11d507c14b239c8650f4f594b006f
Dokkabei97 <wkdrn970@naver.com>
2023-07-24 08:08:30 +0900

    feat: redis 연결 수정 및 table 대문자 -> 소문자로 변경

--- src/main/resources/db/migration/V2__init_dml.sql
+++ src/main/resources/db/migration/V2__init_dml.sql
@@ -1,17 +1,17 @@
 -- 회원 데이터 입력
-INSERT INTO MEMBERS (EMAIL, PASSWORD, NICKNAME, NAME, PROFILE_IMAGE, ROLE)
+INSERT INTO members (email, password, nickname, name, profile_image, role)
 VALUES ('test1@example.com', 'password1', 'test_nick1', 'test_name1', 'image_url1', 'ROLE_MEMBER'),
        ('test2@example.com', 'password2', 'test_nick2', 'test_name2', 'image_url2', 'ROLE_MEMBER'),
        ('test3@example.com', 'password3', 'test_nick3', 'test_name3', 'image_url3', 'ROLE_MEMBER');
 
--- 게시물 데이터 입력
-INSERT INTO POSTS (MEMBER_ID, TITLE, CONTENT, POST_IMAGES)
+-- 게시물 데이터 입력+
+INSERT INTO posts (member_id, title, content, post_images)
 VALUES (1, 'test_title1', 'test_content1', '["image1_url", "image2_url"]'),
        (2, 'test_title2', 'test_content2', '["image3_url", "image4_url"]'),
        (3, 'test_title3', 'test_content3', '["image5_url", "image6_url"]');
 
 -- 게시물 좋아요 데이터 입력
-INSERT INTO POST_LIKES (POST_ID, MEMBER_ID)
+INSERT INTO post_likes (post_id, member_id)
 VALUES (1, 2),
        (1, 3),
        (2, 1),
@@ -19,7 +19,7 @@ VALUES (1, 2),
        (3, 2);
 
 -- 게시물 댓글 데이터 입력
-INSERT INTO POST_COMMENTS (POST_ID, MEMBER_ID, CONTENT)
+INSERT INTO comments (post_id, member_id, content)
 VALUES (1, 2, 'test_comment1'),
        (1, 3, 'test_comment2'),
        (2, 1, 'test_comment3'),
@@ -27,7 +27,7 @@ VALUES (1, 2, 'test_comment1'),
        (3, 2, 'test_comment5');
 
 -- 게시물 댓글 좋아요 데이터 입력
-INSERT INTO POST_COMMENT_LIKES (POST_COMMENT_ID, MEMBER_ID)
+INSERT INTO comment_likes (comment_id, member_id)
 VALUES (1, 1),
        (2, 1),
        (3, 2),
@@ -35,13 +35,13 @@ VALUES (1, 1),
        (5, 3);
 
 -- 북마크 게시물 데이터 입력
-INSERT INTO BOOKMARK_POSTS (POST_ID, MEMBER_ID)
+INSERT INTO bookmarks (post_id, member_id)
 VALUES (1, 1),
        (2, 2),
        (3, 3);
 
 -- 팔로우 데이터 입력
-INSERT INTO FOLLOWS (FOLLOWER_MEMBER_ID, FOLLOWING_MEMBER_ID)
+INSERT INTO follows (follower_member_id, following_member_id)
 VALUES (1, 2),
        (1, 3),
        (2, 1),
